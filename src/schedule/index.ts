4d2302f1a0134071747c252fd1b5f0d422ff9ae0
the-non-feline <32406395+the-non-feline@users.noreply.github.com>
2019-04-14 20:06:57 -0700

    Update index.ts

--- src/schedule/index.ts
+++ src/schedule/index.ts
@@ -46,11 +46,7 @@ export function getScheduleFromDay(month: number, day: number, year: number, wee
   let shed = Schedule.NONE; 
   let date = `${month} - ${day} - ${year}`; 
   
-  
-  
   if(date in special_dates) {
-    
-    
     shed = special_dates[date]; 
   } else {
     console.log('2'); 
