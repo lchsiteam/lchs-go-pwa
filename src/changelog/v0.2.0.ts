c33cc149075092e1a319ebb7558d87b3f660648a
RyantheKing <gamingskills2002@gmail.com>
2019-04-27 16:40:36 -0700

    [add] New changelog, credits change, and new themes

---
+++ src/changelog/v0.2.0.ts
@@ -0,0 +1,21 @@
+export default {
+  title: "v0.2.0", 
+  entries: [
+    {
+      id: 6, 
+      isNew: true, 
+      isPublic: true, 
+      title: 'New Aestheics and Options with themes.', 
+      author: 'Ryan King', 
+      tagline: 'LCHS Go now looks better and with more functionality. (Also read below to see how you can support the LCHS iTeam)' ,
+      content: [
+        { type: 'text', text: 'Donate you old computer to the iTeam: If you wish to help the creators of the LCHS Go app, so that we can continue to make more updates to the website, you can! Our club only has access to a couple computers, which greatly limits our capability to make better changes to the app.'}, 
+        { type: 'text', text: 'If you have an old computer, that you do not use, we would be greatful if you could drop it by the Help Lab, and inform whoever is there that it is a donation to the iTeam. What might not be useful to you, could be a huge asset to our club!'}, 
+        {type: 'text', text: ''}, 
+        {type: 'text', text: 'The themes for LCHS Go now look much better, and include a bigger variety of options.'}, 
+        {type: 'text', text: 'In addition, there is a new "Dynamic Theme", that will change color, depending on the time of day, it can be found in the theme select menu, under settings, like all the other themes.'}, 
+      ], 
+    }, 
+    
+  ]
+}
