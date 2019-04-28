bbd72b872e624f4d062ea837789681cf565dd582
RyantheKing <gamingskills2002@gmail.com>
2019-04-28 14:21:15 -0700

    [fix] cHaNgEelOuGe gRaMmAr IsSuz

--- src/changelog/v0.2.0.ts
+++ src/changelog/v0.2.0.ts
@@ -5,11 +5,11 @@ export default {
       id: 6, 
       isNew: true, 
       isPublic: true, 
-      title: 'New Aestheics and Options with themes.', 
+      title: 'New Aesthetics and Options With Themes', 
       author: 'Ryan King', 
       tagline: 'LCHS Go now looks better and with more functionality. (Also read below to see how you can support the LCHS iTeam)' ,
       content: [
-        { type: 'text', text: 'Donate you old computer to the iTeam: If you wish to help the creators of the LCHS Go app, so that we can continue to make more updates to the website, you can! Our club only has access to a couple computers, which greatly limits our capability to make better changes to the app.'}, 
+        { type: 'text', text: 'Donate your old computer to the iTeam: If you wish to help the creators of the LCHS Go app, so that we can continue to make more updates to the website, you can! Our club only has access to a couple computers, which greatly limits our capability to make better changes to the app.'}, 
         { type: 'text', text: 'If you have an old computer, that you do not use, we would be greatful if you could drop it by the Help Lab, and inform whoever is there that it is a donation to the iTeam. What might not be useful to you, could be a huge asset to our club!'}, 
         {type: 'text', text: ''}, 
         {type: 'text', text: 'The themes for LCHS Go now look much better, and include a bigger variety of options.'}, 
