bcfdae360ea025c0dedc6539e979d6bde32eff6c
not a cat <32406395+the-non-feline@users.noreply.github.com>
2019-05-02 16:20:54 -0700

    [ft] added a warning about grade settings being changed

--- src/changelog/v0.2.0.ts
+++ src/changelog/v0.2.0.ts
@@ -8,6 +8,8 @@ export default {
       author: 'Ryan King', 
       tagline: 'LCHS Go now looks better and with more functionality. (Read below to see how you can support the LCHS iTeam)' ,
       content: [
+        { type: 'text', text: 'ATTENTION: The "Grade Level" setting has been slightly reworked. Remember to make sure you are on the correct grade setting. ', }, 
+        { type: 'text', text: '', }, 
         { type: 'text', text: 'Donate your old computer to the iTeam: If you wish to help the creators of the LCHS Go app, so that we can continue to make more updates to the website, you can! Our club only has access to a couple computers, which greatly limits our capability to make better changes to the app.'}, 
         { type: 'text', text: 'If you have an old computer (even if it does not work) that you do not use, we would be grateful if you could drop it by the Help Lab and inform whoever is there that it is a donation to iTeam. What might not be useful to you, could be a huge asset to our club!'}, 
         { type: 'text', text: ''}, 
