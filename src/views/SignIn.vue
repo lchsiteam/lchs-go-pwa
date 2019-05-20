317bb5ba6d2baf42366176e152bbbc00aa3c5e6a
MC-Atom <40128144+MC-Atom@users.noreply.github.com>
2019-05-20 12:19:09 -0700

    Indenting

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -319,76 +319,75 @@ export default class ChangelogPage extends Vue {
 
 
 // FirebaseUI config.
-      var uiConfig = {
-        callbacks: {
-          signInSuccessWithAuthResult: function(authResult: any, redirectUrl: any) {
-            var user = authResult.user;
-            var credential = authResult.credential;
-            var isNewUser = authResult.additionalUserInfo.isNewUser;
-            var providerId = authResult.additionalUserInfo.providerId;
-            var operationType = authResult.operationType;
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult: any, redirectUrl: any) {
+      var user = authResult.user;
+      var credential = authResult.credential;
+      var isNewUser = authResult.additionalUserInfo.isNewUser;
+      var providerId = authResult.additionalUserInfo.providerId;
+      var operationType = authResult.operationType;
 
-            var userEmail = user.email;
+      var userEmail = user.email;
 
-            var isLcusd = userEmail.indexOf("lcusd.net")
+      var isLcusd = userEmail.indexOf("lcusd.net")
 
-            if (isLcusd == -1) {
-              user.delete();
+      if (isLcusd == -1) {
+        user.delete();
               
-              return false;
-              
-
-            } else {
-              return true;
-            }
+        return false;
+             
+      } else {
+        return true;
+      }
             
-            return false;
-          },
-          signInFailure: function(error: any) {
-            // Some unrecoverable error occurred during sign-in.
-            // Return a promise when error handling is completed and FirebaseUI
-            // will reset, clearing any UI. This commonly occurs for error code
-            // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
-            // occurs. Check below for more details on this.
-            return handleUIError(error);
-          },
-          uiShown: function() {
-            // The widget is rendered.
-            // Hide the loader.
-            (document.getElementById('loader') as any).style.display = 'none';
-          }
-        },
-        credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM,
-        // Query parameter name for mode.
-        queryParameterForWidgetMode: 'mode',
-        // Query parameter name for sign in success url.
-        queryParameterForSignInSuccessUrl: 'signInSuccessUrl',
-        // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-        signInFlow: 'popup',
-        signInSuccessUrl: "arg.html",
-        signInOptions: [
-          // Leave the lines as is for the providers you want to offer your users.
-          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        ],
-        // tosUrl and privacyPolicyUrl accept either url string or a callback
-        // function.
-        // Terms of service url/callback.
-        tosUrl: 'termsOfService.html',
-        // Privacy policy url/callback.
-        privacyPolicyUrl: function() {
-          window.location.assign('privacyPolicy.html');
-        }
-      };
+      return false;
+    },
+  signInFailure: function(error: any) {
+    // Some unrecoverable error occurred during sign-in.
+    // Return a promise when error handling is completed and FirebaseUI
+    // will reset, clearing any UI. This commonly occurs for error code
+    // 'firebaseui/anonymous-upgrade-merge-conflict' when merge conflict
+    // occurs. Check below for more details on this.
+    return handleUIError(error);
+  },
+  uiShown: function() {
+    // The widget is rendered.
+    // Hide the loader.
+    (document.getElementById('loader') as any).style.display = 'none';
+  }
+},
+  credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM,
+  // Query parameter name for mode.
+  queryParameterForWidgetMode: 'mode',
+  // Query parameter name for sign in success url.
+  queryParameterForSignInSuccessUrl: 'signInSuccessUrl',
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: 'popup',
+  signInSuccessUrl: "arg.html",
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+  // tosUrl and privacyPolicyUrl accept either url string or a callback
+  // function.
+  // Terms of service url/callback.
+  tosUrl: 'termsOfService.html',
+  // Privacy policy url/callback.
+  privacyPolicyUrl: function() {
+    window.location.assign('privacyPolicy.html');
+  }
+};
 
-      var ui = new firebaseui.auth.AuthUI(firebase.auth());
-      // The start method will wait until the DOM is loaded.
-      ui.start('#firebaseui-auth-container', uiConfig); 
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-      function handleUIError (error: any) {
+// The start method will wait until the DOM is loaded.
+ui.start('#firebaseui-auth-container', uiConfig); 
+
+function handleUIError (error: any) {
   console.log(error)
   return Promise.reject(error)
 }
-
 
 /* Google Sign In */
 
