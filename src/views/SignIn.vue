c9fb50fd79cf724dc8b5ba9e2ee562260ed439ed
MC-Atom <40128144+MC-Atom@users.noreply.github.com>
2019-05-28 13:08:30 -0700

    Update SignIn.vue

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -364,7 +364,7 @@ var uiConfig = {
   queryParameterForSignInSuccessUrl: 'signInSuccessUrl',
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: "arg.html",
+  signInSuccessUrl: "home",
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
