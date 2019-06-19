ca184e06749ddc41fbbed5e5faf258c5f95c4545
RyantheKing <48844450+RyantheKing@users.noreply.github.com>
2019-06-19 11:43:20 -0700

    Update router.ts

--- src/router.ts
+++ src/router.ts
@@ -11,9 +11,9 @@ export default new Router({
       path: '/home',
       component: Home,
       children: [
-        { path: 'news', component: () => import('./views/News.vue') },
-        { path: 'Welcome', component: () => import('./views/Welcome.vue') },
-        { path: 'Virtualid', component: () => import('./views/Virtualid.vue') },
+        //{ path: 'news', component: () => import('./views/News.vue') },
+        //{ path: 'Welcome', component: () => import('./views/Welcome.vue') },
+        //{ path: 'Virtualid', component: () => import('./views/Virtualid.vue') },
         { path: 'schedule', component: () => import('./views/BellSchedule.vue') },
         { path: '', component: () => import('./views/Now.vue') },
       ]
