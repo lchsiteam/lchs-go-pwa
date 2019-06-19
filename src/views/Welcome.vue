896cf4a66a17b15b053cc26b3064adc58d500cc8
not a cat <32406395+the-non-feline@users.noreply.github.com>
2019-06-19 14:42:19 -0700

    [fix] rolled back my older change

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -26,8 +26,8 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'; 
 import { allGrades } from '@/schedule'; 
-import { firebase } from 'firebase';
-import { firebaseui } from 'firebaseui';
+import firebase from 'firebase';
+import firebaseui from 'firebaseui';
 
 
 @Component({})
