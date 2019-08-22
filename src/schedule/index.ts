f82a6d03dbca7bc6a38cece16e770529f32747d2
MC-Atom <40128144+MC-Atom@users.noreply.github.com>
2019-08-21 20:05:22 -0700

    Adds on to Not A Cat's by updateing index.ts to how it was before everything
    Fixes index.ts so block days exist again.

--- src/schedule/index.ts
+++ src/schedule/index.ts
@@ -119,10 +119,10 @@ export function getScheduleFromDay(month: number, day: number, year: number, wee
             shed = Schedule.REGULAR;
             break;
           case Day.WEDNESDAY:
-            shed = Schedule.REGULAR;//BLOCK_ODD;
+            shed = Schedule.BLOCK_ODD;
             break;
           case Day.THURSDAY:
-            shed = Schedule.REGULAR;//BLOCK_EVEN;
+            shed = Schedule.BLOCK_EVEN;
             break; 
         } 
       } 
@@ -148,10 +148,10 @@ export function getFullSchedule(schedule: Schedule, grade: number): any {
       return high_schooler ? RegularSchedule : RegularSchedule78; 
       break; 
     case Schedule.BLOCK_ODD: 
-      return high_schooler ? RegularSchedule : RegularSchedule78; //return high_schooler ? BlockOddSchedule : BlockOddSchedule78; 
+      return high_schooler ? BlockOddSchedule : BlockOddSchedule78; 
       break; 
     case Schedule.BLOCK_EVEN: 
-      return high_schooler ? RegularSchedule : RegularSchedule78; //return high_schooler ? BlockEvenSchedule : BlockEvenSchedule78; 
+      return high_schooler ? BlockEvenSchedule : BlockEvenSchedule78; 
       break; 
     case Schedule.SBAC_BLOCK_ODD: 
       return high_schooler ? BlockOddSchedule : HSBlockOddScheduleFor78; 
