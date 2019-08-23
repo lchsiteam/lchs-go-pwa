00eb42376b6e775251549187a0066e336666c6bb
not a cat <32406395+the-non-feline@users.noreply.github.com>
2019-08-23 10:19:27 -0700

    test change

--- src/views/BellSchedule.vue
+++ src/views/BellSchedule.vue
@@ -6,7 +6,7 @@
     <p>Click the black arrow to display a calendar selection.</p>
     <label for="start">Enter date:</label>
     <input type="date" id="start" name="day-of-school"
-      :value="this.date"
+      v-model="this.date"
       min="2019-07-01" max="2020-5-3">
     <p>{{this.date}}</p>
     
