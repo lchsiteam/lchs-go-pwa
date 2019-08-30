d849c2c3982a11e38790016aba66aaef718944eb
MC-Atom <40128144+MC-Atom@users.noreply.github.com>
2019-08-30 18:53:46 +0200

    made better

--- src/views/Now.vue
+++ src/views/Now.vue
@@ -58,7 +58,213 @@ export default class Now extends Vue {
   private currentPeriod = { start: 0, end: 1440, period: Period.NONE }; 
   private allLogs: any[] = []
   public useNextPeriodStartAsEnd = false    // TODO: Find a better variable name
-@@ -271,28 +270,25 @@
+  updateStats() {
+    const currentDate = DateTime.local().setZone("America/Los_Angeles").plus(Duration.fromMillis(plus_days * 86400000)); 
+    this.minutes = currentDate.minute + (currentDate.hour * 60); 
+    this.currentDateTime = currentDate
+    this.grade = this.$store.state.settings.grade; 
+    this.schedule = getScheduleFromDay(currentDate.month, currentDate.day, currentDate.year, currentDate.weekday, this.grade); 
+    this.currentPeriod = getPeriod(this.minutes, this.schedule, this.grade); 
+  } 
+  getUnreadUpdates() {
+    return this.allLogs.filter(entry => this.$store.state.changelog.readUpdates.indexOf(entry.id) === -1); 
+  } 
+  goToChangelog() {
+    if (this.$store.state.isExtension) { window.open('/about/changelog', '_blank'); } 
+    else { this.$router.push('about/changelog'); } 
+  }
+  shouldShowUpdateLog() {
+    return this.getUnreadUpdates().length > 0
+  } 
+  //Don't put the period (the punctuation mark one) here. It is supplied in the place where this function is called. 
+  getGreeting() {
+    if (this.minutes <= 330) return "Good late evening" 
+    else if (this.minutes <= 720) return "Good morning" 
+    else if (this.minutes <= 1050) return "Good afternoon" 
+    else if (this.minutes <= 1440) return "Good evening" 
+    else return "Hello, student"
+  }
+  printTime(time: number) {
+    return printTime(time)
+  }
+  getCurrentPeriodName() {
+    return getPeriodName(this.currentPeriod.period)
+  }
+  getFormattedTimeUntilNext() {
+    return [Math.floor(this.getTimeUntilNext()/60), this.getTimeUntilNext() % 60]
+  }
+  getCurrentScheduleName() {
+    return getScheduleName(this.schedule); 
+  }
+  getUpcomingPeriod() {
+    return getUpcomingPeriod(this.minutes, this.currentDateTime, this.schedule, this.grade)
+  }
+  getPeriodEnd() {
+    if (this.useNextPeriodStartAsEnd) {
+      return this.getUpcomingPeriod().start + ((this.getUpcomingPeriod().daysSince || 0) * 1440)
+    } else { return this.currentPeriod.end }
+  }
+  getTimeUntilNext() {
+    return this.getPeriodEnd() - this.minutes
+  }
+  getUnitUntilNext() {
+    return this.getPeriodEnd() - this.minutes >= 120 ? "hr." : "min."
+  }
+  getUntilNextName() {
+    if (!this.useNextPeriodStartAsEnd) {
+      return this.currentPeriod.period === Period.DONE ? "today ends" : "period ends"
+    } else {
+      const nextPeriod = this.getUpcomingPeriod()
+      return nextPeriod ? getPeriodName(nextPeriod.period) : "next day"
+    }
+  }
+  getCurrentTime24() {
+    return ("0000" + Math.floor(this.minutes / 60)).substr(-2) + ":" + ("0000" + (this.minutes % 60)).substr(-2)
+  }
+ 
+  getCurrentTime12() {
+    let end_string = "AM"
+    let hours = Math.floor(this.minutes / 60)
+    let new_hours = (hours % 12 === 0 ? 12 : hours % 12)     // Show 12:00 AM instead of 00:00 AM
+    if (hours >= 12) {
+      end_string = "PM"
+    }
+    return `${new_hours + ":" + ("0000" + (this.minutes % 60)).substr(-2)} ${end_string}`
+  }
+   
+  getCurrentTime() {
+    if(!this.$store.state.settings.useMilitaryTime) {
+      return this.getCurrentTime12()
+    }
+    else {
+      return this.getCurrentTime24()
+    }
+  }
+  getCertainTime12(time: number) {
+    let end_string = "AM"
+    let hours = Math.floor(time / 60)
+    let new_hours = (hours % 12 === 0 ? 12 : hours % 12)     // Show 12:00 AM instead of 00:00 AM
+    if (hours >= 12 && hours <= 23) {
+      end_string = "PM"
+    }
+    return `${new_hours + ":" + ("0000" + (time % 60)).substr(-2)} ${end_string}`
+  }
+  getCertainTime24(time: number) {
+    return ("0000" + Math.floor(time / 60)).substr(-2) + ":" + ("0000" + (time % 60)).substr(-2)
+  }
+  getCertainTime(time: number) {
+    return this.$store.state.settings.useMilitaryTime ? this.getCertainTime24(time) : this.getCertainTime12(time)
+  }
+  getCurrentPercentage() {
+    return 1 - (this.getTimeUntilNext() / (this.getPeriodEnd() - this.currentPeriod.start))
+  }
+  getCurrentTimeParts24() {
+    return {
+      hr: ("0000" + Math.floor(this.minutes / 60)).substr(-2),
+      min: ("0000" + (this.minutes % 60)).substr(-2)
+    }
+  }
+ 
+  getCurrentTimeParts12() {
+    let end_string = "AM"
+    let hours = Math.floor(this.minutes / 60)
+    let new_hours = (hours % 12 === 0 ? 12 : hours % 12)     // Show 12:00 AM instead of 00:00 AM
+    if (hours >= 12) {
+      end_string = "PM"
+    }
+    return {
+      hr: new_hours,
+      min: `${("0000" + (this.minutes % 60)).substr(-2)} ${end_string}`
+    }
+  }
+ 
+  getCurrentTimeParts() {
+    //to do: replace "12hourplaceholder" with object/variable that determines if 12-hour mode is turned on
+    if(!this.$store.state.settings.useMilitaryTime) {
+      return this.getCurrentTimeParts12()
+    }
+    else {
+      return this.getCurrentTimeParts24()
+    }
+  } 
+  
+  updateOptionBL(name: string, value: any): void {
+    this.$store.commit('UPDATE_SETTING', { name, value }); 
+  } 
+  changeGrade(grade: number) {
+    this.updateOptionBL('grade', grade); 
+  } 
+  mounted() {
+    //correct invalid grade settings if any
+    let grade = this.$store.state.settings.grade; 
+    
+    if(allGrades.indexOf(grade) == -1) {
+      grade = allGrades[2]; 
+      
+      this.changeGrade(grade); 
+    } 
+    //note that this didn't make any assignments to this.grade. That's because this part is just to correct invalid settings. 
+    
+    setInterval(this.updateStats, 5000)
+    this.updateStats()
+    Changelog.forEach(version => {
+      this.allLogs = this.allLogs.concat(version.entries)
+    })
+    this.allLogs = this.allLogs.filter(log => log.isPublic) 
+  }
+}
+</script>
+<style lang="scss" scoped>
+.home {
+  padding: 0 20px;
+}
+.cd-num {
+  font-size: 36px;
+}
+.cd-txt {
+  font-size: 18px;
+  margin-left: 8px;
+}
+.cd-txt-h {
+  font-size: 14px;
+  font-weight: 400;
+}
+.grid-fmr {
+  text-align: left;
+  margin: 10px auto;
+  padding: 6px 10px;
+  background-color: rgba(0, 0, 0, .1);
+  border: 1px solid rgba(0, 0, 0, .3);
+  position: relative;
+  .grid-fmr-absmode {
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 6px 12px;
+    background-color: rgba(0, 0, 0, .1);
+    color: rgba(255, 255, 255, 0.7);
+    font-size: 12px;
+    font-weight: 700;
+    transition: 150ms ease;
+  }
+  &:hover > .grid-fmr-absmode {
+    background-color: rgba(0, 0, 0, .15);
+    color: rgba(255, 255, 255, 0.9);
+  }
+}
+.grid-fmr-helper {
+  font-size: 12px;
+  font-weight: 600;
+  opacity: 0.6;
+}
+.grid-fmr-mini-click {
+  background-color: rgba(0, 0, 0, .3);
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+  transition: 100ms ease;
+  &:hover { background-color: rgba(0, 0, 0, .4); }
+  .grid-fmr-value {
     text-align: left;
     font-size: 14px;
   }
