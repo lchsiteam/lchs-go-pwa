2acb49b70cbcda4ad13ff062125a429c77fb13e9
MC-Atom <40128144+MC-Atom@users.noreply.github.com>
2019-09-03 09:04:57 -0700

    Changed full to HUGE in datetime

--- src/views/Now.vue
+++ src/views/Now.vue
@@ -68,7 +68,7 @@ export default class Now extends Vue {
   } 
   
   getCurrentDate() {
-    return DateTime.local().setZone("America/Los_Angeles").plus(Duration.fromMillis(plus_days * 86400000)).toLocaleString(DateTime.DATE_FULL);
+    return DateTime.local().setZone("America/Los_Angeles").plus(Duration.fromMillis(plus_days * 86400000)).toLocaleString(DateTime.DATE_HUGE);
    }
   
   getUnreadUpdates() {
