1843a45b6ffabe9912ffad120f6a9756c6a310b6
MC-Atom <40128144+MC-Atom@users.noreply.github.com>
2019-09-09 14:02:55 -0700

    Moved settings here

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
     <div class="sub-nav">
       <router-link class="sub-nav-item" to="/home" exact>Now</router-link>
       <router-link class="sub-nav-item" to="/home/schedule">Bell Schedule</router-link>
+      <router-link class="sub-nav-item" to="/about/settings">Settings</router-link>
     </div>
     <!--
     <div>
