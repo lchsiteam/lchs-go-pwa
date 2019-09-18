688c0f3133fe5883477381cf294dd1e97545035f
MC-Atom <40128144+MC-Atom@users.noreply.github.com>
2019-09-18 06:28:35 -0700

    asdasd

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 import './registerServiceWorker';
-import VCalendar from 'v-calendar';
 
 Vue.config.productionTip = false;
 
