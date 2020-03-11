f26891bbdd812adafbcee3c3e829758b2d47a095
not a cat <32406395+the-non-feline@users.noreply.github.com>
2020-03-11 11:18:10 -0700

    [fix] fixed style issues

--- src/views/BellSchedule.vue
+++ src/views/BellSchedule.vue
@@ -368,9 +368,9 @@ export default class Home extends Vue {
   }
 
   public changeGrade(grade: number) {
-    this.updateOptionBL('grade', this.grade); 
-    
-    this.updateStats(); 
+    this.updateOptionBL('grade', this.grade);
+
+    this.updateStats();
   }
 
   public mounted() {
