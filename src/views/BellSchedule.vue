f57081270b1ee5e08613e048759e73b60151ffa5
MC-Atom <thepnkprkr@gmail.com>
2020-08-03 17:00:07 -0700

    Changed Pre K  to TK and recess to Recess/PE in K

--- src/views/BellSchedule.vue
+++ src/views/BellSchedule.vue
@@ -311,7 +311,7 @@ export default class Home extends Vue {
       grade = String(grade);
       grade = grade.concat('th Grade');
     } else if (grade === 0) {
-      grade = 'K/Pre K';
+      grade = 'TK/K';
     } else if (grade === 1) {
       grade = '1st Grade';
     } else if (grade === 2) {
