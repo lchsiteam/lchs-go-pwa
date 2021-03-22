e9b3152b443ff29e0a88c1dda366e63cf30d7546
MC-Atom <40128144+MC-Atom@users.noreply.github.com>
2021-03-22 16:57:39 -0700

    Made it so the numbers are gud

--- src/App.vue
+++ src/App.vue
@@ -224,7 +224,7 @@ firebase.initializeApp(firebaseConfig);
 
 @font-face {
   font-family: upsidedown;
-  src: url(./assets/Upsidedown-Regular.ttf);
+  src: url(./assets/UpsidedownNoNumbers-Regular.ttf);
 }
 
 @keyframes AnimatedTheme {
