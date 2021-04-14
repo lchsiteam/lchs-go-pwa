f194926d6a0bf51e8dff81005792efa4c48cc01b
Atom MC <thepnkprkr@gmail.com>
2021-04-14 16:06:07 -0700

    Revert "Testing if a thing works"
    This reverts commit 75fd0fa84012b60e60ad45e645ca1d627e3d3994.

--- src/App.vue
+++ src/App.vue
@@ -222,11 +222,6 @@ firebase.initializeApp(firebaseConfig);
 
 <style lang="scss">
 
-@font-face {
-  font-family: 'Niramit', Avenir, sans-serif;
-  src: url(/src/assets/Upsidedown-Regular.ttf);
-}
-
 @keyframes AnimatedTheme {
   0% { background-position: 50% 0%; }
   50% { background-position: 51% 100%; }
