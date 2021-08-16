872f9cdfea3abcb1df267a3b491793accac31e81
LCHS iTeam <team@lciteam.club>
2021-08-16 11:58:55 -0700

    Revert "Revert "Testing > master""

--- src/schedule/index.ts
+++ src/schedule/index.ts
@@ -150,7 +150,7 @@ export function getScheduleFromDay(month: number, day: number, year: number, wee
         case Day.WEDNESDAY:
         case Day.THURSDAY:
         case Day.FRIDAY:
-          shed = Schedule.SUMMER_SCHOOL;
+          shed = Schedule.REGULAR;
           break;
       }
     } else if (grade === 13) {
