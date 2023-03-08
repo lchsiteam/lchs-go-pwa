c51ed6f2e5f92cc0aefb04f013a09fab945a03ee
Mark Crichton <monika+git@mcrichton.net>
2023-03-08 12:59:47 -0800

    Draft Post

---
+++ src/changelog/v0.3.0.ts
@@ -0,0 +1,27 @@
+export default {
+  title: 'v0.3.0',
+  entries: [
+    {
+      id: 8,
+      isNew: true,
+      isPublic: true,
+      title: 'The Future of LCHS Go',
+      author: 'M. Crichton',
+      tagline: 'New LCHS Go Beta and Planned Deprecation of the current LCHS Go experience' ,
+      content: [
+        { type: 'text', text: 'Hello there! It\'s been a while.'},
+        { type: 'text', text: 'We\'ve been working on a newer, updated version of LCHS Go for some time now.'},
+        { type: 'text', text: 'This updated experience introduces some new features, such as language translation and custom class names!' },
+        { type: 'text', text: 'It would also allow us to change and update the schedule more frequently (such as the pilot schedule proposed eariler this school year).' },
+        { type: 'text', text: 'You can find the updated website and extension using the links below:' },
+        { type: 'call-to-action', text: 'Beta Website', url: 'https://betago.lciteam.club/' },
+        { type: 'call-to-action', text: 'Chrome Extension', url: 'https://chrome.google.com/webstore/detail/lchs-go-beta/bddnpkadgjcbafnmbampfbaiijafealk' },
+        { type: 'text', text: 'However, because of this new experience, we plan on replacing this current website with the new LCHS Go in the future.' },
+        { type: 'text', text: 'The current LCHS Go website continue to be hosted at https://legacygo.lciteam.club, and the current extension will not be replaced.' },
+        { type: 'text', text: 'However, neither will receive any further updates when the new experience is out of beta.' },
+        { type: 'text', text: 'As always, you can let us know what you think by emailing us.' },
+        { type: 'call-to-action', text: 'Send an E-Mail', url: 'mailto:team@lciteam.club' },
+      ],
+    },
+  ],
+};
