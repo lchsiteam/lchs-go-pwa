f2fd4180e11973986616f6e374a952606532a1ad
Mark Crichton <monika+git@mcrichton.net>
2023-04-08 23:28:06 -0700

    Add a link to report issue
    Also change subtitle

--- src/changelog/v0.3.0.ts
+++ src/changelog/v0.3.0.ts
@@ -7,7 +7,7 @@ export default {
       isPublic: true,
       title: 'The Future of LCHS Go',
       author: 'M. Crichton',
-      tagline: 'New LCHS Go Beta and Planned Deprecation of the current LCHS Go experience' ,
+      tagline: 'A new, fresh LCHS Go experience!' ,
       content: [
         { type: 'text', text: 'Hello there! It\'s been a while.'},
         { type: 'text', text: 'We\'ve been working on a newer, updated version of LCHS Go for some time now.'},
@@ -16,6 +16,8 @@ export default {
         { type: 'text', text: 'You can find the updated website and extension using the links below:' },
         { type: 'call-to-action', text: 'Beta Website', url: 'https://betago.lciteam.club/' },
         { type: 'call-to-action', text: 'Chrome Extension', url: 'https://chrome.google.com/webstore/detail/lchs-go-beta/bddnpkadgjcbafnmbampfbaiijafealk' },
+        { type: 'text', text: 'If you find any issues with the beta, you can report them using the link below:' },
+        { type: 'call-to-action', text: 'Report an Issue', url: 'https://forms.gle/BgFW4MgL9RGpezdw7' },
         { type: 'text', text: 'However, because of this new experience, we plan on replacing this current website with the new LCHS Go in the future.' },
         { type: 'text', text: 'The current LCHS Go website continue to be hosted at https://legacygo.lciteam.club, and the current extension will not be replaced.' },
         { type: 'text', text: 'However, neither will receive any further updates when the new experience is out of beta.' },
