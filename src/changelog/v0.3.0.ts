d03b4f2dfac5d5d8d95c2a8c6e9ead6d0b66575e
Mark Crichton <monika+git@mcrichton.net>
2023-04-10 17:58:56 -0700

    Update v0.3.0.ts
    Revert previous commit

--- src/changelog/v0.3.0.ts
+++ src/changelog/v0.3.0.ts
@@ -19,7 +19,7 @@ export default {
         { type: 'text', text: 'If you find any issues with the beta, you can report them using the link below:' },
         { type: 'call-to-action', text: 'Report an Issue', url: 'https://forms.gle/BgFW4MgL9RGpezdw7' },
         { type: 'text', text: 'However, because of this new experience, we plan on replacing this current website with the new LCHS Go in the future.' },
-        { type: 'text', text: 'The current LCHS Go website continue to be hosted at <a href="https://legacygo.lciteam.club">https://legacygo.lciteam.club</a>, and the current extension will not be replaced.' },
+        { type: 'text', text: 'The current LCHS Go website continue to be hosted at https://legacygo.lciteam.club, and the current extension will not be replaced.' },
         { type: 'text', text: 'However, neither will receive any further updates when the new experience is out of beta.' },
         { type: 'text', text: 'As always, you can let us know what you think by emailing us.' },
         { type: 'call-to-action', text: 'Send an E-Mail', url: 'mailto:team@lciteam.club' },
